ea3456519c7fe38cf07d9a8bd672c82af9e26448
Koppolu Maneesha <165755242+KoppoluManeesha@users.noreply.github.com>
2026-03-01 12:22:50 +0530

    Time: 40 ms (36.19%) | Memory: 12.6 MB (98.78%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        count={}
+        for ch in magazine:
+            count[ch]=count.get(ch,0)+1
+        for ch in ransomNote:
+            if ch not in count or count[ch]==0:
+                return False
+            count[ch]-=1
+        return True
+
+        
