77d8a40d1672da0e378e1452c1f8377ba842dc7c
Koppolu Maneesha <165755242+KoppoluManeesha@users.noreply.github.com>
2026-04-17 19:59:56 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (25.39%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def backspaceCompare(self, s, t):
+        def process(string):
+            stack = []
+            for ch in string:
+                if ch == "#":
+                    if stack:
+                        stack.pop()
+                else:
+                    stack.append(ch)
+            return stack
+
+        return process(s) == process(t)
