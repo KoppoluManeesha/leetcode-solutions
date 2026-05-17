6463c741887b747e9fa9bf7a30eea6e669ef92af
Koppolu Maneesha <165755242+KoppoluManeesha@users.noreply.github.com>
2026-05-17 12:30:30 +0530

    Time: 834 ms (6.53%) | Memory: 12.5 MB (81.53%) - LeetSync

---
+++ 2502-sort-the-people/sort-the-people.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def sortPeople(self, names, heights):
+        """
+        :type names: List[str]
+        :type heights: List[int]
+        :rtype: List[str]
+        """
+        for i in range(len(names)):
+            for j in range(i+1,len(names)):
+                if heights[i] < heights[j]:
+                    heights[i],heights[j]=heights[j],heights[i]
+                    names[i],names[j]=names[j],names[i]
+        return names
