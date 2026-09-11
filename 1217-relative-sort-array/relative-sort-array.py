f470d85d3dd4c960d49937480047c4ce201daf92
Koppolu Maneesha <165755242+KoppoluManeesha@users.noreply.github.com>
2026-09-11 10:57:16 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (50.98%) - LeetSync

---
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        freq={}
+        for num in arr1:
+            freq[num]=freq.get(num,0)+1
+        result=[]
+        for num in arr2:
+            if num in freq:
+                result.extend([num]*freq[num])
+                del freq[num]
+        remaining=[]
+        for num in freq:
+            remaining.extend([num]*freq[num])
+        remaining.sort()
+        result.extend(remaining)
+        return result
